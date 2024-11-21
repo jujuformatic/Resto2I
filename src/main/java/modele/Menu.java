571cc4f9d83d1ff6022f6ddc2c6cae5cd1e4decf
Simonpernet <121865777+Simonpernet@users.noreply.github.com>
2024-11-21 09:55:46 +0100

    Add files via upload

---
+++ src/main/java/modele/Menu.java
@@ -0,0 +1,15 @@
+package modele;
+
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity
+public class Menu {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private int Id;
+
+    public Menu() {}
+}
